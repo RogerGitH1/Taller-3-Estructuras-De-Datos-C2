4212852298ddb6b3fbd982b5f2cb68b4e9472650
Roger <roger.villarroel@alumnos.ucn.cl>
2025-12-11 23:06:47 -0300

    feat: Implementación Sistema.h

---
+++ src/Sistema.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "NodoGrafo.h"
+#include "NodoBPlusBase.h"
+
+#include <string>
+
+class Sistema {
+
+private:
+    NodoBPlusBase* raiz;
+    int orden;
+    int siguiente_id;
+
+public:
+    Sistema(int orden);
+    ~Sistema();
+
+    void insertar_nodo_grafo(int clave, NodoGrafo* nodo_grafo);
+
+    NodoGrafo* buscar_nodo_grafo(int clave);
+
+    void crear_nodo(int id_padre);
+
+    void eliminar_archivo(int id_archivo, int id_directorio_padre);
+
+    void listar_contenido(int id_directorio);
+
+    std::string* obtener_rutas_completas(int id_archivo);
+
+    int calcular_espacio_ocupado(int id_directorio);
+};
